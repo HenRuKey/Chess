b7f2806dde1ec69085b07d9956881efed5d44ee8
Keaton Henderson <keaton@sdsix.com>
2018-08-02 13:57:05 -0600

    black and white tiles added

--- Chess/MainWindow.xaml.cs
+++ Chess/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,26 @@ namespace Chess
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Tile> tiles = new List<Tile>();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Color color = (i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1) ? Colors.Black : Colors.White;
+                    Tuple<int, int> position = new Tuple<int, int>(i, j);
+                    Tile tile = new Tile(color, position);
+                    tiles.Add(tile);
+                    gridBoard.Children.Add(tile.Rectangle);
+                }
+            }
         }
     }
 }

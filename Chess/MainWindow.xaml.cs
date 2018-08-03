02cf6807708e700bc7931489a6f05d93bfca4345
Keaton Henderson <keaton@sdsix.com>
2018-08-03 10:55:58 -0600

    updated some colors

--- Chess/MainWindow.xaml.cs
+++ Chess/MainWindow.xaml.cs
@@ -30,11 +30,25 @@ namespace Chess
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            Color softWhite = new Color()
+            {
+                R = 242,
+                B = 242,
+                G = 242,
+                A = 255
+            };
+            Color darkSlate = new Color()
+            {
+                R = 38,
+                B = 38,
+                G = 38,
+                A = 255
+            };
             for (int i = 0; i < 8; i++)
             {
                 for (int j = 0; j < 8; j++)
                 {
-                    Color color = (i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1) ? Colors.Black : Colors.White;
+                    Color color = (i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1) ? darkSlate : softWhite;
                     Tuple<int, int> position = new Tuple<int, int>(i, j);
                     Tile tile = new Tile(color, position);
                     tiles.Add(tile);
